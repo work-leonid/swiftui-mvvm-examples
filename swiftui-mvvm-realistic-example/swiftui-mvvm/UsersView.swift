f441f274626ecdcd377f080ed3f1a5daa75374e2
work-leonid <work.leonid@gmail.com>
2021-05-31 09:51:55 +0300

    full code

--- swiftui-mvvm-realistic-example/swiftui-mvvm/UsersView.swift
+++ swiftui-mvvm-realistic-example/swiftui-mvvm/UsersView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct UsersView: View {
+    
+    @StateObject var viewModel: ViewModel
+    
+    init(viewModel: ViewModel = .init()) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
     var body: some View {
-        Text("Hi")
+        List(viewModel.users) { user in
+            Text(user.name)
+        }
+        .onAppear(perform: viewModel.getUsers)
     }
 }
 
+extension UsersView {
+    class ViewModel: ObservableObject {
+        @Published var users = [User]()
+        
+        let dataService: DataService
+        
+        init(dataService: DataService = AppDataService()) {
+            self.dataService = dataService
+        }
+        
+        func getUsers() {
+            dataService.getUsers { [weak self] users in
+                self?.users = users
+            }
+        }
+    }
+}
 
 struct UsersView_Previews: PreviewProvider {
     static var previews: some View {
-        UsersView()
+        
+        let user = User(id: 0, name: "Preview User")
+        let viewModel = UsersView.ViewModel()
+        viewModel.users = [user]
+        
+        return UsersView(viewModel: viewModel)
     }
 }
--- swiftui-mvvm-realistic-example/swiftui-mvvmTests/swiftui_mvvmTests.swift
+++ swiftui-mvvm-realistic-example/swiftui-mvvmTests/swiftui_mvvmTests.swift
@@ -5,33 +5,36 @@
 //  Created by Kilo Loco on 12/22/20.
 //
 
-//import XCTest
-//@testable import swiftui_mvvm
-//
-//class MockDataService: DataService {
-//    func getUsers(completion: @escaping ([User]) -> Void) {
-//        completion([User(id: 1, name: "Kilo Loco")])
-//    }
-//}
-//
-//class swiftui_mvvmTests: XCTestCase {
-//
-//    var sot: UsersView.ViewModel!
-//    
-//    override func setUpWithError() throws {
-//        sot = UsersView.ViewModel(dataService: MockDataService())
-//    }
-//
-//    override func tearDownWithError() throws {
-//        sot = nil
-//    }
-//    
-//    func test_getUsers() throws {
-//        XCTAssertTrue(sot.users.isEmpty)
-//        
-//        sot.getUsers()
-//        
-//        XCTAssertEqual(sot.users.count, 1)
-//    }
-//
-//}
+import XCTest
+@testable import swiftui_mvvm
+
+class MockDataService: DataService {
+    func getUsers(completion: @escaping ([User]) -> Void) {
+        completion([User(id: 1, name: "Kilo Loco")])
+    }
+    
+    
+}
+
+class swiftui_mvvmTests: XCTestCase {
+    
+    var sut: UsersView.ViewModel!
+    
+    override func setUpWithError() throws {
+//        sut = .init()
+        sut = UsersView.ViewModel(dataService: MockDataService())
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_getUsers() throws {
+        XCTAssertTrue(sut.users.isEmpty)
+        
+        sut.getUsers()
+        
+        XCTAssertEqual(sut.users.count, 1)
+    }
+    
+}
